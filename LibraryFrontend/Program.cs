16e0b57439b4ee5c0498504d9da9157ad0bbf4c3
HamzeHussein <hamze_hussein@hotmail.com>
2025-04-10 16:35:56 +0200

    lagt till adda böcker och funtionerna till det

--- LibraryFrontend/Program.cs
+++ LibraryFrontend/Program.cs
@@ -1,15 +1,33 @@
-using Microsoft.AspNetCore.Components.Web;
+﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using LibraryFrontend;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5235/") });
+// HttpClient + services
+builder.Services.AddHttpClient<BookService>(client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7206/");
+});
+builder.Services.AddScoped<BookService>(); // ← Lägg till detta!
 
-builder.Services.AddScoped<BookService>();
-builder.Services.AddScoped<AuthService>();
+builder.Services.AddHttpClient<AuthService>(client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7206/");
+});
+
+builder.Services.AddHttpClient<BookService>(client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7206/");
+});
+
+
+builder.Services.AddScoped<AuthService>(); // ← Lägg till detta också!
+
 
 
 await builder.Build().RunAsync();
